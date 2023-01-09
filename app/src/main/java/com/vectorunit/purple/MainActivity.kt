97893e71c121481e81f105a28ab28e55ccd83a5d
arsusToday <arslan.ochkhaev@yandex.ru>
2023-01-09 15:26:14 +0600

    added branch, waiting for AF

--- app/src/main/java/com/vectorunit/purple/MainActivity.kt
+++ app/src/main/java/com/vectorunit/purple/MainActivity.kt
@@ -2,9 +2,11 @@ package com.vectorunit.purple
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.facebook.applinks.AppLinkData
 import com.vectorunit.purple.policy.BeamAct
+import io.branch.referral.Branch
 
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +15,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         startActivity(Intent(this, BeamAct::class.java))
+
+        Branch.sessionBuilder(this).withCallback { branchUniversalObject, linkProperties, error ->
+            if (error != null) {
+                Log.e("BranchSDK_Tester", "branch init failed. Caused by -" + error.message)
+            } else {
+                Log.e("BranchSDK_Tester", "branch init complete!")
+                if (branchUniversalObject != null) {
+                    Log.e("BranchSDK_Tester", "title " + branchUniversalObject.title)
+                    Log.e("BranchSDK_Tester", "CanonicalIdentifier " + branchUniversalObject.canonicalIdentifier)
+                    Log.e("BranchSDK_Tester", "metadata " + branchUniversalObject.contentMetadata.convertToJson())
+                }
+                if (linkProperties != null) {
+                    Log.e("BranchSDK_Tester", "Channel " + linkProperties.channel)
+                    Log.e("BranchSDK_Tester", "control params " + linkProperties.controlParams)
+                }
+            }
+        }.withData(this.intent.data).init()
+
 
     }
 }
--- app/src/main/java/com/vectorunit/purple/MainCla.kt
+++ app/src/main/java/com/vectorunit/purple/MainCla.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import com.onesignal.OneSignal
 import com.vectorunit.purple.policy.util.appModule
 import com.vectorunit.purple.policy.util.viewModelModule
+import io.branch.referral.Branch
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.GlobalContext
@@ -14,6 +15,11 @@ class MainCla: Application() {
         super.onCreate()
         OneSignal.initWithContext(this)
         OneSignal.setAppId("f38a0369-f672-4bed-af24-2a19ee3447f2")
+
+        Branch.enableTestMode()
+
+        // Branch object initialization
+        Branch.getAutoInstance(this)
 
         GlobalContext.startKoin {
             androidLogger(Level.DEBUG)
