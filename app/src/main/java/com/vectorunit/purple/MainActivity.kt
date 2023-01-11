ae96cb0c88e8598e39d9a23223afb4cb6e5ac4ec
Marina <marina@Marinas-MacBook-Pro.local>
2023-01-11 18:55:19 +0300

    finish

--- app/src/main/java/com/vectorunit/purple/MainActivity.kt
+++ app/src/main/java/com/vectorunit/purple/MainActivity.kt
@@ -2,24 +2,14 @@ package com.vectorunit.purple
 
 import android.annotation.SuppressLint
 import android.content.Intent
-import android.content.SharedPreferences
 import android.os.Bundle
-import android.util.Log
-import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.vectorunit.purple.MainCla.Companion.C1
-import com.vectorunit.purple.MainCla.Companion.appsCheckChe
-import com.vectorunit.purple.policy.RedirAct
 import com.vectorunit.purple.policy.ViMod
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.core.qualifier.named
-import java.util.concurrent.Executors
-import java.util.concurrent.TimeUnit
 
 
 class MainActivity : AppCompatActivity() {
@@ -41,5 +31,9 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onBackPressed() {
+        super.onBackPressed()
+        finish()
+    }
 
 }
